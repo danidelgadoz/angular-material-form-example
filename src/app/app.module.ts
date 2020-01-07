fb93ce2cd5a5d2f49cb29fd6c2140a392c5e93a8
Daniel Delgado <dedd1993@gmail.com>
2020-01-07 13:46:41 -0500

    style: move shared module inside app folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { SharedModule } from 'src/shared/shared.module';
+import { SharedModule } from 'src/app/shared/shared.module';
 import { AppComponent } from './app.component';
 import { FormExampleComponent } from './form-example/form-example.component';
 
