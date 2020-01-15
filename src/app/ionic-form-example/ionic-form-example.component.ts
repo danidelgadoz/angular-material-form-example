2bc616ae51a8f0535f9c32b47ce623f61c6a8693
Daniel Delgado <dedd1993@gmail.com>
2020-01-15 15:05:21 -0500

    create the ionic form example

---
+++ src/app/ionic-form-example/ionic-form-example.component.ts
@@ -0,0 +1,42 @@
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-ionic-form-example',
+  templateUrl: './ionic-form-example.component.html',
+  styleUrls: ['./ionic-form-example.component.scss']
+})
+export class IonicFormExampleComponent implements AfterViewInit {
+  @ViewChild('dniElemRef', { static: false }) documentNumberElm: any;
+
+  form: FormGroup;
+
+  constructor(
+    public formBuilder: FormBuilder,
+  ) {
+    this.initFormBuilder();
+  }
+
+  ngAfterViewInit() {
+    setTimeout(() => this.documentNumberElm.setFocus() , 1000);
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
+  }
+
+  onReset() {
+    this.form.reset();
+  }
+
+  private initFormBuilder() {
+    this.form = new FormGroup({
+      documentNumber: new FormControl({ value: null, disabled: false }, [ ]),
+      email: new FormControl({ value: null, disabled: false }, [ Validators.required, Validators.email ]),
+      password: new FormControl({ value: null, disabled: false }, [ Validators.required ]),
+      role: new FormControl({ value: null, disabled: false }, [ Validators.required ]),
+      rememberMe: new FormControl({ value: false, disabled: false }, [ ]),
+    });
+  }
+
+}
