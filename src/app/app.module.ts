663d6e9c3d74fcee91601141ddd74b3a7cc828c7
Daniel Delgado <dedd1993@gmail.com>
2020-01-15 15:04:03 -0500

    install ionic core and its angular steroids

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IonicModule } from '@ionic/angular';
 
 import { SharedModule } from 'src/app/shared/shared.module';
 import { AppComponent } from './app.component';
 import { FormExampleComponent } from './form-example/form-example.component';
+import { IonicFormExampleComponent } from './ionic-form-example/ionic-form-example.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormExampleComponent
+    FormExampleComponent,
+    IonicFormExampleComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    IonicModule,
     SharedModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class AppModule { }
